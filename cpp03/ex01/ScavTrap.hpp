9218e13ff4fc5e3745bd944e09002fbe304b2052
Zinebaiterrami1 <zineb.aiterrami1@gmail.com>
2025-11-29 15:17:45 +0100

    ex02

---
+++ cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,21 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include <iostream>
+# include <string>
+# include "ClapTrap.hpp"
+
+class ScavTrap: ClapTrap
+{
+    public:
+        ScavTrap();
+        ScavTrap(std::string name);
+        ScavTrap(const ScavTrap &val);
+        ScavTrap& operator=(const ScavTrap& val);
+        ~ScavTrap();
+
+        void guardGate();
+        void attack(const std::string& target);
+};
+
+#endif
